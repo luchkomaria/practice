ba770bcfc2e6566eb4c1fd1f0dc2626c4a86a0bf
luchkomaria <Luchkomaria@gmail.com>
2017-03-27 00:33:19 +0300

    unfinished

---
+++ Desktop/Practice/JS_4/data.php
@@ -0,0 +1,20 @@
+﻿<?php
+$bg=$_POST['background'];
+$box=$_POST['box'];
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$query = "INSERT INTO colors (background, box) VALUES ('{$bg}', '{$box}')";
+if ($conn->query($query) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>

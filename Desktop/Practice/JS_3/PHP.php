721b4a81b50e3bd0fafd58f281ed0a1ff2539bf4
luchkomaria <Luchkomaria@gmail.com>
2017-03-26 21:35:03 +0300

    JS_3

---
+++ Desktop/Practice/JS_3/PHP.php
@@ -0,0 +1,37 @@
+﻿<?php 
+$fst = $_POST['fst'];
+$nd = $_POST['nd'];
+$action =  $_POST['action'];
+if(!is_numeric($fst)||!is_numeric($nd)){
+	echo "errorn";}
+else{	
+switch ($action) {
+    case "plus":
+		$result = $fst+$nd;
+        echo $result;
+        break;
+    case "minus":
+        $result = $fst-$nd;
+		echo $result;
+        break;
+    case "multiply":
+        $result = $fst*$nd;
+		echo $result;
+        break;
+    case "divide":
+	if($nd==0) 
+	{echo "error0";}
+	else {$result = $fst/$nd;}
+		echo $result;
+        break;
+    case "exponentiation":
+        $result = pow($fst,$nd);
+		echo $result;
+        break;
+	case "remain":
+        $result = $fst % $nd;
+		echo $result;
+        break;
+}
+}
+?>
